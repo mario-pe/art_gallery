02f81d7a086053f436e44b8de7b83416a1b6c038
mperkowski <mperkowski@grupazpr.pl>
2019-09-06 19:44:45 +0200

    dodanie stringow do medelu

--- gallery/art/models.py
+++ gallery/art/models.py
@@ -4,12 +4,18 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=256)
 
+    def __str__(self):
+        return "{}".format(self.name)
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=256)
     second_name = models.CharField(max_length=256)
     birthday = models.DateField(blank=True, null=True)
     description = models.TextField()
+
+    def __str__(self):
+        return "{}, {}".format(self.first_name, self.second_name)
 
 
 class Product(models.Model):
@@ -21,3 +27,6 @@ class Product(models.Model):
 
     def get_authors(self):
         return ",".join([str(p) for p in self.authors.all()])
+
+    def __str__(self):
+        return "{}, {}".format(self.title, self.category)
